8743c7e786b5c076e8e767fb5d3af4df2ced2fb6
Aric12 <12aaron.chong@gmail.com>
2024-07-05 00:33:30 +0800

    fix all bugs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-//import SignUpBox from './SignUpBox/Signupbox.js'
-import LogInBox from './LogInBox/LogInBox.js'
+import SignUpBox from './SignUpBox/Signupbox.js'
+//import LogInBox from './LogInBox/LogInBox.js'
 //import Main2 from './MainMenu/Main2.js'
 
 
 function App(){
   return (
     <div>
-      {/* <SignUpBox /> */}
-      <LogInBox /> 
+      <SignUpBox />
+      {/* <LogInBox /> */}
       {/* <Main2 /> */}
     </div>
   )
--- src/SignUpBox/Signupbox.js
+++ src/SignUpBox/Signupbox.js
@@ -33,28 +33,25 @@ export default function SignUpBox(){
   return (
     <>
     <div className={style.container}>
-     <form onSubmit={handleSubmit}>
+      <div className={style.box}>
+        <form onSubmit={handleSubmit}>
       
-      <h2 className={style.words}>Sign Up now !!!</h2>
+          <h2 className={style.words}>Sign Up now !!!</h2>
       
-      <label className={style.words} for='username'>Username:</label>
-        <br/><input type="text" id="username" name="username" value={formData.Username} onChange={handleChange} required></input><br/>
+          <label className={style.words} for='username'>Username:</label>
+          <br/><input type="text" id="username" name="username" value={formData.Username} onChange={handleChange} required></input><br/>
 
-      <label className={style.words} for='email'>Email: </label><br/>
-         <input type="email" id="email" name="email" value={formData.Email} onChange={handleChange} required></input><br/>
+          <label className={style.words} for='email'>Email: </label><br/>
+          <input type="email" id="email" name="email" value={formData.Email} onChange={handleChange} required></input><br/>
         
-      <label className={style.words} for='password'>Password: </label><br/>
-         <input type="password" id="password" name="password" value={formData.Password} onChange={handleChange} required></input><br/>
+          <label className={style.words} for='password'>Password: </label><br/>
+          <input type="password" id="password" name="password" value={formData.Password} onChange={handleChange} required></input><br/>
 
-      <p className={style.words}>Password must be at least 9 characters for security purposes, contain at least one digit, and one alphabetic character. By signing up, you agreed to our General Terms</p>
+          <p className={style.words}>Password must be at least 9 characters for security purposes, contain at least one digit, and one alphabetic character. By signing up, you agreed to our General Terms</p>
      
-     <button type='submit'>
-       Sign Up
-     </button>
-     
-     
-     
-     </form>
+          <button type='submit'>Sign Up</button>
+        </form>
+      </div>
     </div>
     </>
   )
